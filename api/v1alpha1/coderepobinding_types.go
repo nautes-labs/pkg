c92b7b15c646fd6a9d9a82fb71849b5ea21f5eef
luojinxu <luojinxu@vispractice.com>
2023-04-28 02:49:03 +0000

    feat: add CodeRepoBinding CRD definition.

---
+++ api/v1alpha1/coderepobinding_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// CodeRepoBindingSpec defines the desired state of CodeRepoBinding
+type CodeRepoBindingSpec struct {
+	// Authorized Code Repository.
+	CodeRepo string `json:"coderepo,omitempty"`
+	// The Code repo is authorized to this product or projects under it.
+	Product string `json:"product,omitempty"`
+	// If the project list is empty, it means that the code repo is authorized to the product.
+	// If the project list has values, it means that the code repo is authorized to the specified projects.
+	// +optional
+	Projects []string `json:"projects,omitempty"`
+	// Authorization Permissions, readwrite or readonly.
+	Permissions string `json:"permissions,omitempty"`
+}
+
+// CodeRepoBindingStatus defines the observed state of CodeRepoBinding
+type CodeRepoBindingStatus struct {
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// CodeRepoBinding is the Schema for the coderepobindings API
+type CodeRepoBinding struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CodeRepoBindingSpec   `json:"spec,omitempty"`
+	Status CodeRepoBindingStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// CodeRepoBindingList contains a list of CodeRepoBinding
+type CodeRepoBindingList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CodeRepoBinding `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CodeRepoBinding{}, &CodeRepoBindingList{})
+}
